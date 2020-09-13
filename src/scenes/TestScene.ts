1e13ba43032db4ecfaf74e8f8a12e46a705df36e
Joe Gasparich <joe.gasparich@accedo.tv>
2020-09-13 21:00:12 +1200

    Delete unused tings

--- src/scenes/TestScene.ts
+++
@@ -1,8 +0,0 @@
-import { Scene } from "engine";
-
-import { Assets } from "consts";
-
-export default class TestScene extends Scene {
-    public name = "Test";
-    public mapFile = Assets.MAPS.TEST;
-}
